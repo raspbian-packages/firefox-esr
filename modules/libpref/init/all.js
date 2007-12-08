8161093464ab396ccb3fabfd0e850382333a5460
Mike Hommey <glandium@debian.org>
2007-12-08 19:24:40 +0100

    Set javascript.options.showInConsole
    Gbp-Pq: Topic prefs
    Gbp-Pq: Name Set-javascript.options.showInConsole.patch

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1139,6 +1139,7 @@ pref("javascript.options.strict.debug",     false);
 #endif
 pref("javascript.options.baselinejit",      true);
 pref("javascript.options.ion",              true);
+pref("javascript.options.showInConsole",    true);
 pref("javascript.options.asmjs",            true);
 pref("javascript.options.native_regexp",    true);
 pref("javascript.options.parallel_parsing", true);
