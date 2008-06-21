14a08c388e92c5a1e6c88a683552e372ef8a6e78
Mike Hommey <glandium@debian.org>
2008-06-21 03:09:21 +0200

    Add another preferences directory for applications: preferences/syspref
    It was existing in previous versions of iceweasel as a symlink to
    /etc/iceweasel/pref.
    
    This has the side effect to make these preferences there work again, and
    to disable the "set as default browser" dialog.
    
    Gbp-Pq: Topic debian-hacks
    Gbp-Pq: Name Add-another-preferences-directory-for-applications-p.patch

--- toolkit/xre/nsXREDirProvider.cpp
+++ toolkit/xre/nsXREDirProvider.cpp
@@ -762,6 +762,7 @@ static nsresult DeleteDirIfExists(nsIFile* dir) {
 
 static const char* const kAppendPrefDir[] = {"defaults", "preferences",
                                              nullptr};
+static const char *const kAppendSysPrefDir[] = { "defaults", "syspref", nullptr };
 
 #ifdef DEBUG_bsmedberg
 static void DumpFileArray(const char* key, nsCOMArray<nsIFile> dirs) {
@@ -795,6 +796,7 @@ nsresult nsXREDirProvider::GetFilesInternal(const char* aProperty,
 
     LoadDirIntoArray(mXULAppDir, kAppendPrefDir, directories);
     LoadDirsIntoArray(mAppBundleDirectories, kAppendPrefDir, directories);
+    LoadDirIntoArray(mXULAppDir, kAppendSysPrefDir, directories);
 
     rv = NS_NewArrayEnumerator(aResult, directories);
   } else if (!strcmp(aProperty, NS_APP_CHROME_DIR_LIST)) {
