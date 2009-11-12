d487ed9266041be476c2cd46c6486ee84efb3116
Mike Hommey <mh@glandium.org>
2009-11-12 17:18:31 +0100

    Avoid wrong sessionstore data to keep windows out of user sight
    It happens that sessionstore can keep wrong information for the window
    position or minimized state. Workaround this until the real bug is found
    and fixed (probably in xulrunner).
    
    Closes: #552426, #553453
    
    Gbp-Pq: Topic debian-hacks
    Gbp-Pq: Name Avoid-wrong-sessionstore-data-to-keep-windows-out-of.patch

--- browser/components/sessionstore/SessionStore.sys.mjs
+++ browser/components/sessionstore/SessionStore.sys.mjs
@@ -6589,7 +6589,7 @@ var SessionStoreInternal = {
         // _getWindowDimension.
         let desktopToCssScale =
           aWindow.desktopToDeviceScale / aWindow.devicePixelRatio;
-        aWindow.moveTo(aLeft * desktopToCssScale, aTop * desktopToCssScale);
+        aWindow.moveTo((aLeft < -aWidth) ? 0 : aLeft * desktopToCssScale, (aTop < -aHeight) ? 0 : aTop * desktopToCssScale);
       }
       if (
         aWidth &&
@@ -6617,9 +6617,8 @@ var SessionStoreInternal = {
           case "minimized":
             if (aSizeModeBeforeMinimized == "maximized") {
               aWindow.maximize();
+              break;
             }
-            aWindow.minimize();
-            break;
           case "normal":
             aWindow.restore();
             break;
