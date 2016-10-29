aa88d4c4e7b1247a3c892cb8609a33b85e56fff3
Mike Hommey <mh@glandium.org>
2016-10-29 18:23:56 +0900

    Fix crashes in AtomicOperations-none on s390x
    Gbp-Pq: Topic porting
    Gbp-Pq: Name Fix-crashes-in-AtomicOperations-none-on-s390x.patch

--- js/src/jit/AtomicOperations.h
+++ js/src/jit/AtomicOperations.h
@@ -341,6 +341,9 @@ AtomicOperations::isLockfree(int32_t size)
 #  include "jit/none/AtomicOperations-ppc.h"
 # elif defined(__aarch64__)
 #  include "jit/arm64/AtomicOperations-arm64.h"
+# elif defined(__s390__)
+  // The sparc file is more or less generic and works on s390x.
+#  include "jit/none/AtomicOperations-sparc.h"
 # else
 #  include "jit/none/AtomicOperations-none.h" // These MOZ_CRASH() always
 # endif
