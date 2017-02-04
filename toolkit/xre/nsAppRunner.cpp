4d2bf8e3ec2189f28c5fb0824e0c1be2de991639
Mike Hommey <mh@glandium.org>
2017-02-05 08:41:22 +0900

    Set program name from the remoting name
    Gbp-Pq: Topic debian-hacks
    Gbp-Pq: Name Set-program-name-from-the-remoting-name.patch

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -3990,7 +3990,7 @@ XREMain::XRE_mainStartup(bool* aExitFlag)
 
   // Set program name to the one defined in application.ini.
   {
-    nsAutoCString program(gAppData->name);
+    nsAutoCString program(gAppData->remotingName);
     ToLowerCase(program);
     g_set_prgname(program.get());
   }
