8379d1e8e8583f3db1d60623026a46e78dff4480
Mike Hommey <mh@glandium.org>
2017-04-02 06:33:19 +0900

    Use the Mozilla Location Service key when the Google Key is not there
    Gbp-Pq: Topic debian-hacks
    Gbp-Pq: Name Use-the-Mozilla-Location-Service-key-when-the-Google.patch

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1394,7 +1394,7 @@ pref("dom.debug.propagate_gesture_events_through_content", false);
 
 // All the Geolocation preferences are here.
 //
-#ifndef EARLY_BETA_OR_EARLIER
+#if !defined(EARLY_BETA_OR_EARLIER) && defined(GOOGLE_LOCATION_SERVICE_API_KEY)
 pref("geo.wifi.uri", "https://www.googleapis.com/geolocation/v1/geolocate?key=%GOOGLE_LOCATION_SERVICE_API_KEY%");
 #else
 // Use MLS on Nightly and early Beta.
