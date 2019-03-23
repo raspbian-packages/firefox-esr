25291965245158f27a1f2fca953a9fdd20a603a3
Mike Hommey <glandium@debian.org>
2019-03-23 23:15:11 +0000

    Import firefox-esr_60.6.1esr-1~deb9u1.debian.tar.xz
    [dgit import tarball firefox-esr 60.6.1esr-1~deb9u1 firefox-esr_60.6.1esr-1~deb9u1.debian.tar.xz]

---
+++ vendor.js
@@ -0,0 +1,8 @@
+pref("distribution.searchplugins.defaultLocale", "en-US");
+// Forbid application updates
+pref("app.update.enabled", false, locked);
+
+pref("devtools.webide.autoinstallADBHelper", false);
+pref("devtools.webide.autoinstallFxdtAdapters", false);
+
+pref("spellchecker.dictionary_path", "/usr/share/hunspell");
