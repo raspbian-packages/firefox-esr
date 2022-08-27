7ffa688922afa2a37e20b36ce264e1c7c3dd442f
Peter Michael Green <plugwash@raspbian.org>
2022-08-27 08:00:25 +0000

    Disable more armv7/neon stuff.
    Gbp-Pq: Name disable-more-armv7neon-stuff.patch

--- third_party/aom/aom_dsp/aom_dsp.cmake
+++ third_party/aom/aom_dsp/aom_dsp.cmake
@@ -96,12 +96,12 @@ list(APPEND AOM_DSP_COMMON_INTRIN_AVX2
             "${AOM_ROOT}/aom_dsp/x86/intrapred_avx2.c"
             "${AOM_ROOT}/aom_dsp/x86/blend_a64_mask_avx2.c")
 
-list(APPEND AOM_DSP_COMMON_INTRIN_NEON
-            "${AOM_ROOT}/aom_dsp/arm/fwd_txfm_neon.c"
-            "${AOM_ROOT}/aom_dsp/arm/loopfilter_neon.c"
-            "${AOM_ROOT}/aom_dsp/arm/intrapred_neon.c"
-            "${AOM_ROOT}/aom_dsp/arm/subtract_neon.c"
-            "${AOM_ROOT}/aom_dsp/arm/blend_a64_mask_neon.c")
+#list(APPEND AOM_DSP_COMMON_INTRIN_NEON
+#            "${AOM_ROOT}/aom_dsp/arm/fwd_txfm_neon.c"
+#            "${AOM_ROOT}/aom_dsp/arm/loopfilter_neon.c"
+#            "${AOM_ROOT}/aom_dsp/arm/intrapred_neon.c"
+#            "${AOM_ROOT}/aom_dsp/arm/subtract_neon.c"
+#            "${AOM_ROOT}/aom_dsp/arm/blend_a64_mask_neon.c")
 
 list(APPEND AOM_DSP_COMMON_INTRIN_DSPR2
             "${AOM_ROOT}/aom_dsp/mips/common_dspr2.c"
