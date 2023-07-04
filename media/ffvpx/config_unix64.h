4035d6ebf8d7589d51de2931b34c13084a10326d
Mike Hommey <mh@glandium.org>
2023-07-05 05:25:35 +0900

    Work around https://sourceware.org/bugzilla/show_bug.cgi?id=30578
    Gbp-Pq: Topic debian-hacks
    Gbp-Pq: Name Work-around-https-sourceware.org-bugzilla-show_bug.c.patch

--- media/ffvpx/config_unix64.h
+++ media/ffvpx/config_unix64.h
@@ -204,7 +204,11 @@
 #define HAVE_SYNC_VAL_COMPARE_AND_SWAP 1
 #define HAVE_CABS 0
 #define HAVE_CEXP 0
+#ifdef FFMPEG_DISABLE_INLINE_ASM
+#define HAVE_INLINE_ASM 0
+#else
 #define HAVE_INLINE_ASM 1
+#endif
 #define HAVE_SYMVER 1
 #define HAVE_X86ASM 1
 #define HAVE_BIGENDIAN 0
