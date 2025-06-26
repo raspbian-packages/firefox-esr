73faca29810a78131c5383a3e02aa8e845392819
Peter Michael Green <plugwash@raspbian.org>
2025-06-26 16:45:55 +0000

    fix build

--- js/src/jit/shared/AtomicOperations-shared-jit.cpp
+++ js/src/jit/shared/AtomicOperations-shared-jit.cpp
@@ -55,7 +55,8 @@ static bool UnalignedAccessesAreOK() {
 #  if defined(__x86_64__) || defined(__i386__)
   return true;
 #  elif defined(__arm__)
-  return !HasAlignmentFault();
+  //return !HasAlignmentFault();
+  return false;
 #  elif defined(__aarch64__)
   // This is not necessarily true but it's the best guess right now.
   return true;
